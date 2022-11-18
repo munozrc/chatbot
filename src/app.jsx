905a864462b86db61b8125374a7890de145c30ff
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-18 08:15:36 -0500

    Show and select posible requests

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import { matchPattern } from './helpers/matcher'
 
 export default function App () {
   const [messageHistory, setMessageHistory] = useState([])
+  const [possibleRequests, setPossibleRequests] = useState([])
   const inputElement = useRef(null)
 
   const handleSubmit = (event) => {
@@ -14,15 +15,33 @@ export default function App () {
     setMessageHistory(prev => prev.concat(`<cliente> ${message}`))
     inputElement.current.value = ''
 
-    const { response } = matchPattern(message)
+    const { response, requests } = matchPattern(message)
     setMessageHistory(prev => prev.concat(`<bot> ${response}`))
+    requests && setPossibleRequests(requests)
+  }
+
+  const handleClickRequest = (event) => {
+    const possibleRequest = event.target.innerText
+    setMessageHistory(prev => prev.concat(`<cliente> ${possibleRequest}`))
+    inputElement.current.value = ''
+
+    const { response, requests } = matchPattern(possibleRequest)
+    setMessageHistory(prev => prev.concat(`<bot> ${response}`))
+    requests && setPossibleRequests(requests)
   }
 
   return (
     <main>
       <div>
         <section>
-          { messageHistory.map((message, index) => <p key={index}>{message}</p>) }
+          { messageHistory.map((message, index) => (
+            <p key={`${message}-${index}`}>{message}</p>
+          ))}
+        </section>
+        <section>
+          { possibleRequests.map((request, index) => (
+            <button key={`${request}-${index}`} onClick={handleClickRequest}>{request}</button>
+          )) }
         </section>
         <form onSubmit={handleSubmit}>
           <input
