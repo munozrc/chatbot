3c197ab54832fd704caecb8a52c121017a60a631
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-17 21:36:48 -0500

    Add first pattern

---
+++ src/helpers/patterns.js
@@ -0,0 +1,7 @@
+export default {
+  gretting: {
+    regex: /hola|que\stal|buen+([oa]+s|\s+(dia|tarde|noche))|como\sesta/i,
+    response: '¡Hola! Encantado de conocerte.',
+    requests: []
+  }
+}
