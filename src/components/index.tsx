8f69998c6e90f3d85cdd430a581dcb9b15682acc
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-19 14:40:06 -0500

    Create new component Message

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { ChatHeader } from './chat-header'
 export { FormInput } from './form-input'
+export { Message } from './message'
 export { MessageList } from './message-list'
---
+++ src/components/message/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+import styles from './styles.module.css'
+
+interface MessageProps {
+  children: ReactNode
+  emitter: 'user' | 'bot'
+}
+
+export function Message ({ children, emitter = 'bot' }: MessageProps) {
+  return (
+    <div className={`${styles.wrapper} ${styles['wrapper' + emitter]}`}>
+      <div className={`${styles.container} ${styles['container' + emitter]}`}>
+        {children}
+      </div>
+    </div>
+  )
+}
