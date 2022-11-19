cf4ebc27edcd3079545c5ad9c20c74497303b1cb
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-19 14:57:56 -0500

    feature(Root): Create proccessMessage function

--- src/chatbot.tsx
+++ src/chatbot.tsx
@@ -1,8 +1,21 @@
-import { ChatHeader, FormInput, MessageList } from '@/components'
+import { ChatHeader, FormInput, Message, MessageList } from '@/components'
+import { useContext } from 'react'
+import { ChatbotContext } from './context/chatbot-context'
 
+import allIntentions from './intentions'
+import { defaultIntention } from './intentions/default'
 import styles from './styles/chatbot.module.css'
 
 export default function ChatBot () {
+  const { messageHistory, addMessage } = useContext(ChatbotContext)
+
+  const proccesMessage = (text: string) => {
+    addMessage(<Message emitter='user'>{text}</Message>)
+    const intention = allIntentions.find((obj) => obj.validator(text))
+    const message = intention?.messageAsText ?? defaultIntention.messageAsText
+    addMessage(<Message emitter='bot'>{message}</Message>)
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.shadeWrapper}>
@@ -10,11 +23,11 @@ export default function ChatBot () {
           <ChatHeader agentName='ChatBot' />
           <MessageList
             listReplies={[]}
-            messageHistory={''}
+            messageHistory={messageHistory}
             onClickQuickReply={() => {}}
           />
           <footer className={styles.footer}>
-            <FormInput onSubmitMessage={() => {}}/>
+            <FormInput onSubmitMessage={proccesMessage}/>
             <p>by <a href='https://github.com/munozrc'>@munozrc</a></p>
           </footer>
         </div>
