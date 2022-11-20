78d8ded34acfc1c1c1e99e7e1afc1f062dbd941f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-20 13:47:31 -0500

    feat(Root): Add support to valid entities

--- src/context/chatbot-context.tsx
+++ src/context/chatbot-context.tsx
@@ -1,7 +1,7 @@
 import { createContext, ReactNode, useCallback, useState } from 'react'
-import allIntents, { defaultIntent } from '@/intents'
+import { matchPattern, searchIntentByName } from '@/helpers'
+import { CurrentIntent, Entity, Intent } from '@/types'
 import { Message } from '@/components'
-import { Entity, Intent } from '@/types'
 
 interface ChatbotContextInterface {
   messageHistory: ReactNode
@@ -13,20 +13,14 @@ interface ChatbotProviderProps {
   children: ReactNode
 }
 
+const initValuesCurrentIntent = { data: {}, entity: 0 } as CurrentIntent
+
 export const ChatbotContext = createContext({} as ChatbotContextInterface)
 
 export default function ChatbotProvider ({ children }: ChatbotProviderProps) {
   const [messageHistory, setMessageHistory] = useState<ReactNode>(null)
   const [listReplies, setListReplies] = useState<Array<string>>([])
-  const [currentIntent, setCurrentIntent] = useState<Intent | null>(null)
-  const [currentEntity, setCurrentEntity] = useState<number>(0)
-
-  function getIntentByText (message: string) : Intent | undefined {
-    const intent = allIntents.find((obj) => obj.pattern.test(message))
-    setListReplies(intent?.quickReplies ?? [])
-    setCurrentIntent(intent ?? null)
-    return intent
-  }
+  const [current, setCurrent] = useState<CurrentIntent | null>(null)
 
   function addMessage (message: string, emitter: 'bot' | 'user') {
     setMessageHistory(prevMessages => (
@@ -37,38 +31,46 @@ export default function ChatbotProvider ({ children }: ChatbotProviderProps) {
     ))
   }
 
-  const processEntity = useCallback((text: string): void => {
-    const intent = currentIntent as Intent
-    const entities = intent.entities as Array<Entity>
-    const entity = entities[currentEntity] as Entity
-    const nextPosition = currentEntity + 1
-    const isTheLastEntity = entities.length - 1 >= nextPosition
+  const processEntity = useCallback((rawText: string): void => {
+    const { intent, entity: entityPosition, data } = current as CurrentIntent
+    const entities = intent.entities as Entity[]
+    const nextPosition = entityPosition + 1
 
-    if (!entity.pattern.test(text)) return addMessage(entity.errorMessage, 'bot')
+    const { pattern, errorMessage } = entities.at(entityPosition) as Entity
 
-    addMessage(entity.message, 'bot')
+    if (!pattern.test(rawText)) return addMessage(errorMessage, 'bot')
+    if (nextPosition >= entities.length) return setCurrent(null)
 
-    if (isTheLastEntity) return setCurrentEntity(nextPosition)
+    const nextEntity = entities.at(entityPosition) as Entity
+    addMessage(nextEntity.message, 'bot')
+    setCurrent({ intent, data, entity: nextPosition })
+  }, [current])
 
-    setCurrentIntent(null)
-    setCurrentEntity(0)
-  }, [currentIntent, currentEntity])
+  const proccesIntent = useCallback((intent: Intent) : void => {
+    const { message: replyMessage, quickReplies, entities } = intent
 
-  const processNewMessage = useCallback((text: string): void => {
-    addMessage(text, 'user')
+    addMessage(replyMessage, 'bot')
+    setListReplies(quickReplies ?? [])
 
-    if (currentIntent && currentIntent.entities) return processEntity(text)
+    if (!entities) return
 
-    const intent = getIntentByText(text) ?? defaultIntent
-    const trigger = intent.trigger
+    const firstEntity = entities[initValuesCurrentIntent.entity]
+    setCurrent({ ...initValuesCurrentIntent, intent })
+    addMessage(firstEntity.message, 'bot')
+  }, [])
 
-    addMessage(intent.message, 'bot')
+  const processNewMessage = useCallback((rawText: string): void => {
+    addMessage(rawText, 'user')
+    const isIntentionInProgress = current && current.intent.entities
+    if (isIntentionInProgress) return processEntity(rawText)
 
-    if (!trigger) return
+    const matchIntent = matchPattern(rawText)
+    proccesIntent(matchIntent)
 
-    const { message } = getIntentByText(trigger) as Intent
-    addMessage(message, 'bot')
-  }, [currentIntent, processEntity])
+    if (!matchIntent.trigger) return
+    const matchNextIntent = searchIntentByName(matchIntent.trigger)
+    proccesIntent(matchNextIntent)
+  }, [proccesIntent, processEntity, current])
 
   return (
     <ChatbotContext.Provider
