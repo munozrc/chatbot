0d7a207a7f1845dc3eeb3cca926f6ca5c8898235
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-11-20 13:44:33 -0500

    feat(Root): Add new intent

--- src/intents/index.tsx
+++ src/intents/index.tsx
@@ -1,9 +1,29 @@
+import { Intent } from '@/types'
 import { gretting, showMenu } from './greeting'
 export { defaultIntent } from './default'
 
-const allIntents = [
+const allIntents: Array<Intent> = [
   gretting,
-  showMenu
+  showMenu,
+  {
+    name: 'factura',
+    message: 'Para consultar su factura. Realizaremos algunas preguntas...',
+    pattern: /factura/i,
+    entities: [
+      {
+        name: 'identificacion',
+        pattern: /12345/i,
+        message: 'Digite su numero de identificaion sin espacios.',
+        errorMessage: 'Ingrese una identificacion valida por favor.'
+      },
+      {
+        name: 'fecha',
+        pattern: /11\/2022/i,
+        message: 'Escriba la fecha a consultar con el formato MM/AAAA. Ejemplo: 11/2022',
+        errorMessage: 'Ingrese una fecha valida por favor.'
+      }
+    ]
+  }
 ]
 
 export default allIntents
